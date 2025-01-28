e1de61c944084f18193ed97ad3e2a7b3b57c727e
Alexander Nesterenko <nester.era@gmail.com>
2025-01-28 23:37:48 +0300

    Storing user data in DB

--- components/hero.jsx
+++ components/hero.jsx
@@ -31,7 +31,7 @@ const HeroSection = () => {
     <div className="pb-20 px-4">
         <div className="container mx-auto text-center">
             <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
-                Управляйте Финансами <br /> Интеллектуально
+                Управляйте Финансами <br /> с Интеллектом
             </h1>
             <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
                 Платформа финансового менеджмента на базе ИИ, помогающая отслеживать,
